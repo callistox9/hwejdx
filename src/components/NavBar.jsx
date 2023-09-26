f2a018a5c48cbdd0b933637ea006fc6787ebac9a
callistox9 <codecallisto@gmail.com>
2023-09-26 18:46:52 +0530

    css-applied

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar()
     return(
         <nav>
             <img src="/src/images/react_logo.png"></img>
-        <h3 >React Facts</h3>
+        <h3>React Facts</h3>
         <h4>React Course Project 1</h4>
         </nav>
     )
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 export default function Main()
 {
   return (
-    <div className='main'>    
-      <h1>Fun Facts About</h1>
+    <div >    
+      <h1 className='main'>Fun Facts About</h1>
+      <ul className="facts">
+        <li>Was released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+      </ul>
 </div>
 
 
